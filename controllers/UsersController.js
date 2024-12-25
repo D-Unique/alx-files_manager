0133980bda9f8e468c029667e6e61a851009b0a0
Unigwe <emmunigwe@gmail.com>
2024-12-25 17:06:47 +0100

    added return statement

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,33 +1,26 @@
-import cryto from 'crypto';
+import sha1 from 'sha1';
 import dbclient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { password } = req.body;
-    const { email } = req.body;
+    const { email, password } = req.body;
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
-    const eQuery = { email };
-    const dbUserC = await dbclient.dbClient.collection('users');
-    const eCursor = await dbUserC.findOne(eQuery);
-    console.log(`ecursor: ${eCursor}`);
-    if (eCursor !== null) {
-      res.status(400).json({ error: 'Already exist' });
+    const eCursor = await dbclient.dbClient.collection('users').findOne({ email });
+    if (eCursor) {
+      return res.status(400).json({ error: 'Already exist' });
     }
-    const hashPassword = cryto.createHash('SHA1')
-      .update(password)
-      .digest('hex');
+    const hashPassword = sha1(password);
     const newUser = {
       email,
       password: hashPassword,
     };
-    const obj = await dbUserC.insertOne(newUser);
-    res.status(201).json(`{id: ${obj.insertedId}, email: ${email}}`);
-    res.end();
+    const obj = await dbclient.dbClient.collection('users').insertOne(newUser);
+    return res.status(201).json(`{id: ${obj.insertedId}, email: ${email}}`);
   }
 }
 
